de6bd49803864f4ad73774c644571b33dada27ee
KaioFerreira1 <kaiof2902@gmail.com>
2025-05-09 19:47:23 -0300

    Projeto feito e estrutura basica feita

---
+++ src/test/java/com/onlinestore/storebackend/OnlineStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.storebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
